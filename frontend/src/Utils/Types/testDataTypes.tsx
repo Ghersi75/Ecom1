9c0658a3d953da6ebadb2e323701e5a96da2fd3a
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-08-04 10:32:39 -0400

    Updated data type to single item, and changed testData type to a list of testDataType

--- frontend/src/Utils/Types/testDataTypes.tsx
+++ frontend/src/Utils/Types/testDataTypes.tsx
@@ -1,17 +1,15 @@
-export type testDataType = [
-  {
-    "section": string,
-    "items": [
-      {
-        "name": string,
-        "description": string,
-        "price": string | {
-          "sm"?: string,
-          "md"?: string,
-          "xl"?: string
-        },
-        "img"?: string
-      }
-    ]
-  }
-]
+export type testDataType = {
+  "section": string,
+  "items": [
+    {
+      "name": string,
+      "description": string,
+      "price": string | {
+        "sm"?: string,
+        "md"?: string,
+        "xl"?: string
+      },
+      "img"?: string
+    }
+  ]
+}
--- frontend/src/Utils/testData.tsx
+++ frontend/src/Utils/testData.tsx
@@ -1,6 +1,36 @@
 import { testDataType } from "Types/testDataTypes";
 
-const menuData: testDataType = [
+export const menuData: testDataType[] = [
+  {
+    "section": "Popular",
+    "items": [
+      {
+        "name": "Italia Special",
+        "description": "Pizza topped with arugula, prosciutto di parma, and parmesan",
+        "price": {
+          "sm": "$14.99",
+          "md": "19.99",
+          "xl": "24.99"
+        },
+        "img": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1200,height=672,format=auto/https://doordash-static.s3.amazonaws.com/media/photosV2/5b9dd350-7e68-40a2-b014-451d328e82a1-retina-large.JPG",
+      }
+    ]
+  },
+  {
+    "section": "Popular",
+    "items": [
+      {
+        "name": "Italia Special",
+        "description": "Pizza topped with arugula, prosciutto di parma, and parmesan",
+        "price": {
+          "sm": "$14.99",
+          "md": "19.99",
+          "xl": "24.99"
+        },
+        "img": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1200,height=672,format=auto/https://doordash-static.s3.amazonaws.com/media/photosV2/5b9dd350-7e68-40a2-b014-451d328e82a1-retina-large.JPG",
+      }
+    ]
+  },
   {
     "section": "Popular",
     "items": [
@@ -17,5 +47,3 @@ const menuData: testDataType = [
     ]
   }
 ]
-
-export {}
