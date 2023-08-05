8b525c18896f3ad775140db6dc34858ac6c34f26
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-08-05 10:42:19 -0400

    Added navbar opacity

--- frontend/src/Components/Navbar/index.tsx
+++ frontend/src/Components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <div className="fixed h-16 w-screen bg-green-500">
+    <div className="fixed h-16 w-screen bg-green-500 opacity-80">
       <div className="flex h-full flex-row justify-between items-center px-5">
         <div>
           Home
