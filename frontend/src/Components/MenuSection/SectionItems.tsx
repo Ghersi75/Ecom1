4ef5de92d5db0086215263d932d44def2aecff89
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-08-05 10:43:20 -0400

    Updated menu section to show name, description, price, and image of item

--- frontend/src/Components/MenuSection/SectionItems.tsx
+++ frontend/src/Components/MenuSection/SectionItems.tsx
@@ -9,8 +9,25 @@ function SectionItems(props: testDataType) {
       <div className="grid grid-cols-2">
         {props.items.map((item, index) => {
           return(
-            <div className="p-4 bg-red-500 rounded-lg">
-              {item.name} {index}
+            <div className="bg-red-500 rounded-lg flex flex-row justify-between items-center">
+              <div className="flex flex-col">
+                <h1 className="ml-4">
+                  {item.name} {index}
+                </h1>
+                <h1 className="ml-4">
+                  {item.description} {index}
+                </h1>
+                {Object.entries(item.price).map(([size, value], index) => {
+                  // 'size' will be one of 'sm', 'md', or 'xl', and 'value' will be the corresponding price
+                  return (
+                    <div className="ml-4 text-sm" key={index}>
+                      {size}: {value}
+                    </div>
+                  );
+                })}
+              </div>
+              
+              <img src={item.img} className="h-[100%] w-[200px] object-contain"/>
             </div>
           )
         })}
--- frontend/src/Components/MenuSection/index.tsx
+++ frontend/src/Components/MenuSection/index.tsx
@@ -5,7 +5,7 @@ import SectionItems from "Components/MenuSection/SectionItems"
 // props: MenuSectionPropsType
 function MenuSection() {
   return (
-    <div className="grid grid-cols-1 h-96 bg-green-300">
+    <div className="grid grid-cols-1 h-auto bg-green-300">
       {
         testData.map((item, index) => {
           return <SectionItems {...item} key={index} />
