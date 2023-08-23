825986bdab925a8daf86ea44c70091a16f74dd6e
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-08-23 11:22:37 -0400

    switched from testData to dynamo data

--- frontend/src/Components/MenuSection/index.tsx
+++ frontend/src/Components/MenuSection/index.tsx
@@ -1,13 +1,29 @@
 // import { MenuSectionPropsType } from "Types/PropsTypes/MenuSectionPropsType"
 import { testData } from "Utils/testData"
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+import { dataAtom } from "lib/atom"
 import SectionItems from "Components/MenuSection/SectionItems"
 
 // props: MenuSectionPropsType
-function MenuSection() {
+function MenuSection(props: any) {
+  const [data, _] = useAtom(dataAtom);
+
+  useEffect(() => {
+    // console.log(data);
+  }, [data]);
+  
+  console.log(data)
   return (
-    <div className="grid grid-cols-1 h-auto bg-green-300">
+    <div className="grid grid-cols-1 h-auto gap-9 bg-green-300 col-start-1 col-end-4">
+      {/* {
+        data.map((item: any, index: number) => {
+          return <p> {item.section_id} </p>
+        })
+      } */}
       {
-        testData.map((item, index) => {
+        data[0] && 
+        data.map((item: any, index: any) => {
           return <SectionItems {...item} key={index} />
         })
       }
