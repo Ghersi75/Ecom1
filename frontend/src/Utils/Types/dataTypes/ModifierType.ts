a632ec86b9099813b9c8dbc8f57b104d5e7312cb
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-08-23 15:13:19 -0400

    Created modifier type for dynamo data

--- frontend/src/Utils/Types/dataTypes/ModifierType.ts
+++ frontend/src/Utils/Types/dataTypes/ModifierType.ts
@@ -1,5 +1,18 @@
 type ModifierType = {
-  
+  "header": string,
+  "required": boolean,
+  "type": "radio" | "checkbox",
+  "choices": {
+    "name": string,
+    "price": {
+      "sm"?: string,
+      "md"?: string,
+      "lg"?: string,
+      "xl"?: string,
+      "default"?: string
+    },
+    "sorting_key": string
+  }[]
 }
 
 export type { ModifierType }
