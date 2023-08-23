d315a610be992d3ba4e74e240d126648c53544ef
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-08-23 11:23:12 -0400

    added item selected menu and dark background

--- frontend/src/Components/AddItemMenu/index.tsx
+++ frontend/src/Components/AddItemMenu/index.tsx
@@ -1,10 +1,49 @@
 import react from "react"
+import { useAtom } from "jotai"
+import { addMenuItemActive, addMenuItemInformation } from "lib/atom"
 
-function AddItemMenu() {
+function AddItemMenu(props: any) {
+  const [active, setActive] = useAtom(addMenuItemActive)
+  const [menuInfo, setMenuInfo] = useAtom(addMenuItemInformation)
+
+  const onClick = () => {
+    setActive(false)
+
+    console.log("Clicked", active)
+  }
+
+  const BG = (props: any) => {
+    return(
+      <div className={`fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[100vw] h-[100vh] bg-slate-800 opacity-50 z-[5] cursor-pointer ${active ? "" : "hidden"}`} onClick={onClick}>
+  
+      </div>
+    )
+  }
+
+  console.log(menuInfo)
+
   return(
-    <>
-      Overlay page
+    <> 
+      <BG />
+      <div className={`fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[780px] h-[90vh] bg-slate-50 z-10 opacity-100 rounded-xl flex flex-col ${active ? "" : "hidden"}`}>
+        
+        <div className="flex items-center justify-between h-[80px] rounded-t-xl bg-slate-200 px-[20px] font-black text-3xl">
+          {menuInfo && menuInfo?.name?.toUpperCase()}
+          <div className="p-[10px] rounded-full hover:cursor-pointer" onClick={onClick}>
+            X
+          </div>
+        </div>
+        
+        <div className="flex flex-grow flex-col bg-red-200 overflow-y-auto ">
+
+        </div>
+        
+        <div className="flex items-center justify-between h-[80px] w-[100%] rounded-b-xl bg-slate-200 px-[20px] font-black text-3xl">
+          {menuInfo && `$${menuInfo?.price}`}
+        </div>
+      </div>
     </>
+    
   )
 }
 
