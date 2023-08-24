356f08cdc555b6816ead5e4683e87157346361f7
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-08-23 21:28:22 -0400

    updated fetch link to new api with query params

--- frontend/src/Pages/App/index.tsx
+++ frontend/src/Pages/App/index.tsx
@@ -11,31 +11,25 @@ function App() {
 
   const fetchData = async () => { // renamed to avoid shadowing the fetch global
     try {
-      const response = await fetch('https://q6kyb4s3p1.execute-api.us-east-1.amazonaws.com/Test'); // replace with your API endpoint
+      const params = ["modifiers", "sections", "menuItems"]
+      const link = "https://zypflt0q86.execute-api.us-east-1.amazonaws.com/test"
+      const query = link +  "?tables=" + params.join(",")
+
+      console.log(query)
+      const response = await fetch(query);
+
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
+
       const resData = await response.json();
+      setData(resData)
+      console.log(data)
+      // console.log(`Res data`)
       // console.log(resData)
-
-      // Additional safety check for the type
-      try {
-        const responseBody = JSON.parse(resData.body);
-        console.log(responseBody)
-        if (Array.isArray(responseBody)) {
-          const itemList = responseBody || [];
-          setData(itemList);
-          // console.log(itemList);
-        } else {
-          console.error("Unexpected response format.");
-        }
-      } catch (error) {
-        console.error("Error parsing response body:", error);
-      }
-
     } catch (error) {
       if (error instanceof Error) {
-          console.error("There was a problem with the fetch operation:", error.message);
+          console.error("There was a problem with the fetch operation:  ", error.message);
       } else {
           console.error("There was a problem with the fetch operation:", error);
       }
