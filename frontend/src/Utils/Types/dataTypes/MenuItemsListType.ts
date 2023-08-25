50b42f02e18d88129371dd551fbde9e5b927b89b
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-08-24 20:00:05 -0400

    updated item_ids type to string[]

--- frontend/src/Utils/Types/dataTypes/MenuItemsListType.ts
+++ frontend/src/Utils/Types/dataTypes/MenuItemsListType.ts
@@ -3,7 +3,7 @@ import { GenericDataType } from "./GenericDataType"
 type MenuItemsListType = {
   "section_id": string,
   "sorting_key": number,
-  "item_ids": GenericDataType[]
+  "item_ids": string[]
 }
 
 export type { MenuItemsListType }
