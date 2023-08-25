1163a7e7f34b1b4716c4c6abe2a2d783c0da7dee
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-08-24 21:49:48 -0400

    added data types for dataAtom

--- frontend/src/lib/atom.ts
+++ frontend/src/lib/atom.ts
@@ -1,6 +1,15 @@
 import { atom } from "jotai"
+import { GenericDataType } from "Utils/Types/dataTypes/GenericDataType"
+import { MenuItemsListType } from "Utils/Types/dataTypes/MenuItemsListType"
+import { ModifierType } from "Utils/Types/dataTypes/ModifierType"
 
-const dataAtom = atom<any>([])
+type dataAtomType = {
+  "sections": MenuItemsListType[],
+  "modifiers": ModifierType,
+  "menuItems": GenericDataType
+}
+
+const dataAtom = atom<dataAtomType | undefined>(undefined)
 const addMenuItemActive = atom<boolean>(false)
 const addMenuItemInformation = atom<any>({})
 
