723d25e34667f13eec3acbde8c0b7ce7b090cb07
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-15 21:33:58 -0400

    create users sign in/up routes

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserAuthDto } from './dto/userAuth.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post("/signin")
+  signin(@Body() dto: UserAuthDto) {
+    return JSON.stringify(dto)
+  }
+
+  @Post("/signup")
+  async signup(@Body() dto: UserAuthDto) {
+
+  }
+}
