72db2cc2616fb8028c06d516bc65278a56beb8bb
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-16 14:44:15 -0400

    updated sign in credentials

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -9,17 +9,15 @@ export class UsersService {
   }
 
   async signinCredentials(dto: UserCredentialsAuthDto) {
-    if (dto.provider !== "credentials") {
-      const user = await this.database.connection.selectFrom("users").selectAll().where("email", "=", dto.email).executeTakeFirst()
-    
-      // If no user with current email is found, but user logged in with a provider, simply sign up with this email and any given info
-      if (!user) {
-
-        // return new BadRequestException("No user with given email found")
-      }
+    const user = await this.database.connection.selectFrom("users").selectAll().where("email", "=", dto.email).executeTakeFirst()
   
-      return JSON.stringify(user)
+    // If no user with current email is found, but user logged in with a provider, simply sign up with this email and any given info
+    if (!user) {
+
+      return new BadRequestException("No user with given email found")
     }
+
+    return JSON.stringify(user)
     
   }
 
