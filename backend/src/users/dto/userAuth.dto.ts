fb142c356d746616d74aee3cadb271f83d6207d0
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-15 21:33:42 -0400

    created basic user auth dto

---
+++ backend/src/users/dto/userAuth.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator"
+import { IsPasswordRequiredBasedOnProvider } from "./userAuthValidators"
+
+// https://dev.to/sarathsantoshdamaraju/nestjs-and-class-validator-cheat-sheet-13ao#:~:text=%3A%20string%0A%7D-,Specific%20String,-//%20example.dto.ts
+
+// When a user sign in, they may either sign in with they email and password, or simply email and outside provider through next auth
+// Email and provider are always required, and password only required when provider == "credentials"
+export class UserAuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  provider: "credentials" | "google" | "facebook";
+
+  // This is optional only if provider is not "credentials", but this can be checked in the rest of the logic
+  // Important fields are email and provider
+  // Check if password is required or not based on provider
+  @IsPasswordRequiredBasedOnProvider()
+  // @IsString()
+  password: string
+}
