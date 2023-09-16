01b33998b7abd84a94a11df4fe88c74e73debd09
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-16 19:40:56 -0400

    Created navbar with some buttons for testing

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import { Button } from "@/components/ui/button"
+import LogoutButton from './LogoutButton'
+
+export default function() {
+  return(
+    <div className="w-screen h-16 bg-primary-foreground fixed top-0 left-0 flex justify-center items-center gap-4">
+      {/* navbar */}
+      <Link href="/">
+        <Button>
+          Home
+        </Button>
+      </Link>
+      <Link href="/auth/signUp">
+        <Button>
+        Sign Up
+        </Button>
+      </Link>
+      <Link href="/auth/signIn">
+        <Button>
+          Login
+        </Button>
+      </Link>
+      <LogoutButton />
+    </div>
+  )
+}
