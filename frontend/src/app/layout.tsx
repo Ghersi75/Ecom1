9c742bbb5c61d75240ef2b79dfa7804202401136
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-16 19:39:20 -0400

    Added  auth provider and navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar/Navbar'
+import AuthProvider from '@/context/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Pizzeria Website',
   description: 'Online ordering website for pizzeria',
-  
 }
 
 export default function RootLayout({
@@ -17,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`dark ${inter.className}`}>
+        <AuthProvider>  
+          <Navbar />
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   )
 }
