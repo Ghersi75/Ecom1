e3a970908ded1f3f8738c00e1be6371755999e15
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-16 19:40:26 -0400

    Created rough sign up page

--- frontend/src/app/auth/signUp/page.tsx
+++ frontend/src/app/auth/signUp/page.tsx
@@ -1,7 +1,120 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form"
+import Image from "next/image"
+import { signIn } from "next-auth/react"
+import Link from "next/link"
+
+const formSchema = z.object({
+  email: z
+  .string()
+  .email()
+  .min(1, { message: "Email is required"})
+  .max(75, { message: "Email cannot be more than 75 characters long"}),
+  username: z
+    .string()
+    .min(1, { message: "Username is required" })
+    .min(8, { message: "Username must be at least 8 characters long" })
+    .max(32, { message: "Username cannot be longer than 32 characters"}),
+  password: z
+    .string()
+    .min(1, { message: "Password is required" })
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(32, { message: "Password cannot be more than 32 characters long"})
+    .regex(/(?=.*[A-Z])/, { message: "Password must include at least 1 upper case letter"})
+    .regex(/(?=.*[0-9])/, { message: "Password must include at least 1 digit 0-9"})
+    .regex(/(?=.*[!#$%^&*])/, { message: "Password must include at least 1 of the following special characters !#$%^&*"}),
+  confirmPassword: z
+    .string()
+    .min(1, { message: "Password is requied" })
+})
+  .refine((data) => data.password === data.confirmPassword , {
+    path: ["confirmPassword"],
+    message: "Passwords do not match"
+  })
+
 export default function SignUp() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <>
-      Sign Up
-    </>
+    <div className="sm:w-96 w-auto bg-secondary p-8 rounded-lg">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          {/* Email */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Username */}
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Username" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Password */}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full">Sign Up</Button>
+          <p className="text-sm w-full flex justify-center">
+            Already have an account? 
+            <Link href="/auth/signIn" className="ml-1 text-blue-500 hover:underline">
+              Sign In
+            </Link>
+          </p>
+        </form>
+      </Form>
+    </div>
   )
 }
