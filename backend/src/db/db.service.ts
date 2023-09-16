97f55f9e7938b8a623e49f4f6b7e695157f7c44a
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-15 21:49:29 -0400

    created simple db service

--- backend/src/db/db.service.ts
+++ backend/src/db/db.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import type { Kysely } from "kysely"
+import { DB } from './types';
 
 @Injectable()
-export class DbService {}
+export class DbService {
+  constructor(@Inject('DATABASE_CONNECTION') private db: Kysely<DB>) {
+
+  }
+
+  get connection() {
+    return this.db;
+  }
+}
