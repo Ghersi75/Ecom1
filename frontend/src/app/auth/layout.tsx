d35b669680b91bad8a634e163333e3f3295d7020
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-16 19:40:00 -0400

    Removed background and text colors to theme works

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="w-screen h-screen bg-black text-white flex items-center justify-center">
+    <div className="w-screen h-screen flex items-center justify-center">
       {children}
     </div>
   )
