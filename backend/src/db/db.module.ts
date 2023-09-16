25f30a067c5342cd44c889dcd1e80d96ebdcf458
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-15 21:33:06 -0400

    removed db provider that was accidentally created

--- backend/src/db/db.module.ts
+++ backend/src/db/db.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DbController } from './db.controller';
-import { Db } from './db';
 import { DbService } from './db.service';
 
 @Module({
   controllers: [DbController],
-  providers: [Db, DbService]
+  providers: [DbService]
 })
 export class DbModule {}
