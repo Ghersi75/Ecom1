d37f05827b1d16c895977e90bc2753c6b6ade615
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-15 20:42:58 -0400

    Added global validation pipe for nest

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    // This removes the fields/elements that aren't defined
+    // For example, if a description is passed on login, that description wont make it to the controller/service
+    whitelist: true
+  }))
+
   await app.listen(3333);
 }
 bootstrap();
