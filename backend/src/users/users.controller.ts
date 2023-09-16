7a614bc1505c958691b03dcb91e47786d8abea2e
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-15 21:49:45 -0400

    created simple test signin fetch of user

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserAuthDto } from './dto/userAuth.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post("/signin")
   signin(@Body() dto: UserAuthDto) {
-    return JSON.stringify(dto)
+    return this.usersService.signin()
   }
 
   @Post("/signup")
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private database: DbService) {
+
+  }
+
+  async signin() {
+    const user = await this.database.connection.selectFrom("users").selectAll().executeTakeFirst()
+    
+    return JSON.stringify(user)
+  }
+}
