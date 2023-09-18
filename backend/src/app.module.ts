3574d4b1e298e8a64863e9e634903988d8b5271b
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-18 17:18:05 -0400

    Created menu module, controller and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DbModule } from './db/db.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-  imports: [UsersModule, DbModule],
+  imports: [UsersModule, DbModule, MenuModule],
   controllers: [AppController],
   providers: [AppService],
 })
