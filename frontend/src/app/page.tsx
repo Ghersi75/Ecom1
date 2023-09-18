4fc8a6bacf22ada5f709272aaf4c7ece346d6d5b
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-18 17:16:57 -0400

    Added server side data fetching for sections

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,27 @@
-import Image from 'next/image'
+async function getSections() {
+  const res = await fetch("http://localhost:3333/menu/section/", {
+    method: "GET",
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    return null
+    // throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
 
-export default function Home() {
+export default async function Home() {
+  const sections = await getSections()
+
+  console.log(sections)
+
   return (
     <main className="">
-      Website
+      Test
     </main>
   )
 }
