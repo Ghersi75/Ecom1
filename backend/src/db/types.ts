65925b98a8c2ce27aa16ca28f10d8395945bb54f
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-18 17:07:02 -0400

    Added sections to schema and types

--- backend/src/db/types.ts
+++ backend/src/db/types.ts
@@ -4,6 +4,34 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
+export type MenuItems = {
+  item_id: Generated<number>;
+  name: string;
+  display_name: string;
+  description: string | null;
+  image_link: string | null;
+  base_price: string | null;
+  display_order: Generated<number>;
+  is_active: Generated<number>;
+  is_available: Generated<number>;
+  is_featured: Generated<number>;
+  section_id: number;
+};
+export type Section = {
+  section_id: Generated<number>;
+  name: string;
+  display_name: string;
+  display_order: Generated<number>;
+  is_active: Generated<number>;
+  is_available: Generated<number>;
+};
+export type SectionItems = {
+  combo_id: Generated<number>;
+  display_order: Generated<number>;
+  is_active: Generated<number>;
+  is_available: Generated<number>;
+  section_id: number;
+};
 export type User = {
   user_id: Generated<number>;
   username: string;
@@ -18,5 +46,8 @@ export type User = {
   last_login: Generated<Timestamp>;
 };
 export type DB = {
+  menu_items: MenuItems;
+  SectionItems: SectionItems;
+  sections: Section;
   users: User;
 };
