6bbe8d1a58a18f3316fa6a322c386d2227784506
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-09-26 15:24:12 -0400

    Created ViewItemsProvider file for future use

---
+++ frontend/src/context/ViewItemProvider.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { useContext, createContext } from 'react'
+
+type MenuItem = {
+  
+}
+
+
+export default function ViewItemProvider() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
