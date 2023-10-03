30c7e610066c98ec42da756e39640b6536b01247
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-02 20:29:34 -0400

    updated state types for selected

--- frontend/src/lib/types/stateTypes.ts
+++ frontend/src/lib/types/stateTypes.ts
@@ -6,6 +6,7 @@
 export interface ViewItemsRadioSelectedStateInterface {
   type: "RADIO",
   selected_id: number,
+  display_text: string,
   price: ViewItemPriceType
 }
 
@@ -13,14 +14,16 @@ export interface ViewItemsCheckboxSelectedStateInterface {
   type: "CHECKBOX",
   selected_ids: {
     [option_id: number]: {
-      selected: boolean,
+      display_text: string
       price: ViewItemPriceType
     }
   }
 }
 
+export type ViewItemsModifierType = ViewItemsCheckboxSelectedStateInterface | ViewItemsRadioSelectedStateInterface
+
 export interface ViewItemsSelectedStateInterface {
-  [modifierId: number]: (ViewItemsCheckboxSelectedStateInterface | ViewItemsRadioSelectedStateInterface)
+  [modifierId: number]: ViewItemsModifierType
 }
 
 export type ViewItemPriceType = number | {
