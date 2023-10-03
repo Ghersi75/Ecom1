e1d1838f07c37bc2a3a54833ebe6adbf5aebdb73
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-02 20:28:56 -0400

    Updated z index to push nav above everything else

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import LightDarkButton from './LightDarkModeButton'
 
 export default function() {
   return(
-    <div className="w-screen h-16 bg-primary-foreground fixed top-0 left-0 flex justify-center items-center gap-4">
+    <div className="w-screen h-16 z-10 bg-primary-foreground fixed top-0 left-0 flex justify-center items-center gap-4">
       {/* navbar */}
       <Link href="/">
         <Button>
