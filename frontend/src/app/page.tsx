3ebf37fd4dfea2879033a10b4a31ea292e2f1a83
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-02 20:28:40 -0400

    Added shopping cart component on the right

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import ShoppingCart from "@/components/ShoppingCart/ShoppingCart";
 import {
   Card,
   CardContent,
@@ -72,46 +73,49 @@ export default async function Home({
         null
       }
 
-      {
-        sections && sections?.length > 0 ? 
-        <div className="flex flex-col gap-4 p-8 lg:p-16">
-          {sections.map((section, index) => {
-            return (
-              <div key={index}>
-                <p className="text-primary text-lg font-bold">{section?.display_text?.toUpperCase()}</p>
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 pt-4"> {/* Make this a flex container with wrap */}
-                  {section.items && 
-                    section.items.map((item: any, _: any) => {
-                    console.log(item)
-                    return(
-                      <Link
-                        href={`/?productId=${item.item_id}`}>
-                        <Card className="flex flex-row justify-between hover:border-primary hover:cursor-pointer h-44" key={encodeURIComponent(item.item_id)}>
-                          <div className="flex flex-col justify-between">
-                            <CardHeader>
-                              <CardTitle>{item.display_text}</CardTitle>
-                              <CardDescription>{item.description}</CardDescription>
-                            </CardHeader>
-                            {
-                              (item.base_price || item.display_price) && 
-                              <CardFooter>
-                                <p>{`Price ${item.base_price ? formatCurrency(item.base_price) : formatCurrency(item.display_price)}`}</p>
-                              </CardFooter>
-                            }
-                          </div>
-                          <Image className="rounded-r-lg" alt={`Image of ${item.display_name}`} src={`https://i0.wp.com/picjumbo.com/wp-content/uploads/yummy-and-tasty-salami-pizza-diavola-close-up-free-photo.jpg?w=600&quality=80`} width={200} height={200}/>
-                        </Card>
-                      </Link>
-                    )
-                  })}
+      <div className="flex flex-row">
+        {
+          sections && sections?.length > 0 ? 
+          <div className="flex flex-col grow gap-4 p-8 lg:p-16">
+            {sections.map((section, index) => {
+              return (
+                <div key={index}>
+                  <p className="text-primary text-lg font-bold">{section?.display_text?.toUpperCase()}</p>
+                  <div className="grid grid-cols-1 xl:grid-cols-2 gap-4 pt-4"> {/* Make this a flex container with wrap */}
+                    {section.items && 
+                      section.items.map((item: any, _: any) => {
+                      console.log(item)
+                      return(
+                        <Link
+                          href={`/?productId=${item.item_id}`}>
+                          <Card className="flex flex-row justify-between hover:border-primary hover:cursor-pointer h-44" key={encodeURIComponent(item.item_id)}>
+                            <div className="flex flex-col justify-between">
+                              <CardHeader>
+                                <CardTitle>{item.display_text}</CardTitle>
+                                <CardDescription>{item.description}</CardDescription>
+                              </CardHeader>
+                              {
+                                (item.base_price || item.display_price) && 
+                                <CardFooter>
+                                  <p>{`Price ${item.base_price ? formatCurrency(item.base_price) : formatCurrency(item.display_price)}`}</p>
+                                </CardFooter>
+                              }
+                            </div>
+                            <Image className="rounded-r-lg" alt={`Image of ${item.display_name}`} src={`https://i0.wp.com/picjumbo.com/wp-content/uploads/yummy-and-tasty-salami-pizza-diavola-close-up-free-photo.jpg?w=600&quality=80`} width={200} height={200}/>
+                          </Card>
+                        </Link>
+                      )
+                    })}
+                  </div>
                 </div>
-              </div>
-            )
-          })}
-        </div> 
-        : 
-        null
-      }
+              )
+            })}
+          </div> 
+          : 
+          null
+        }
+        <ShoppingCart />
+      </div>
     </main>
 )
 
