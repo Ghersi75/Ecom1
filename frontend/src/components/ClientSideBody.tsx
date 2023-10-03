7eaf95009400651f223c7b907b50b6f811fa34c9
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-03 15:00:16 -0400

    Added option for default props to be passed in

--- frontend/src/components/ClientSideBody.tsx
+++ frontend/src/components/ClientSideBody.tsx
@@ -1,20 +1,24 @@
 "use client"
 
+import { HTMLProps, ReactNode } from "react"
 import { Inter } from 'next/font/google'
 import { useAtom } from "jotai"
 import { darkMode } from "@/lib/atoms"
 
 const inter = Inter({ subsets: ['latin'] })
 
+interface PropsInterface extends HTMLProps<HTMLDivElement> {
+  children: ReactNode
+}
+
 export default function Body({
+  className,
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: PropsInterface) {
   const [dark, _] = useAtom(darkMode)
 
   return(
-    <body className={`${dark ? "dark" : "light"} ${inter.className}`}>
+    <body className={`${dark ? "dark" : "light"} ${className} ${inter.className}`}>
       {children}
     </body>
   )
