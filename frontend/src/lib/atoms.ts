a58d08ee06001e78bda2135b1fb209cc91a9280e
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-02 20:30:31 -0400

    Added cart and modify cart selected atoms

--- frontend/src/lib/atoms.ts
+++ frontend/src/lib/atoms.ts
@@ -1,3 +1,22 @@
 import { atom } from "jotai"
+import { ViewItemsSelectedStateInterface } from "@/lib/types/stateTypes"
 
 export const darkMode = atom<boolean>(true)
+
+export type ShoppingCartType = {
+  selected: ViewItemsSelectedStateInterface
+  quantity: number
+  header: string
+  // Already formatted
+  price: string
+  item_id: string
+}
+
+export const shoppingCart = atom<ShoppingCartType[]>([])
+
+export type ModifyCartItemSelectedType = {
+  selected: ViewItemsSelectedStateInterface,
+  cart_index: number
+}
+
+export const modifyCartItemSelected = atom<ModifyCartItemSelectedType | null>(null)
