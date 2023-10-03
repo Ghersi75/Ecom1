64003f0170943ad7eda6f031010351304416fcde
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-02 20:31:26 -0400

    Updated prop type and selected logic for options

--- frontend/src/components/ViewItem/ViewItemCheckbox.tsx
+++ frontend/src/components/ViewItem/ViewItemCheckbox.tsx
@@ -14,89 +14,99 @@ export default function ViewItemCheckbox({
  } : {
   modifier: ViewItemModifierInterface;
   modifier_option: BaseModifierOptionsInterface;
-  handleChange: (modifier_id: number, option_id: number, price: ViewItemPriceType) => void;
+  handleChange: (modifier_id: number, option_id: number, display_text: string, price: ViewItemPriceType) => void;
   selected: ViewItemsCheckboxSelectedStateInterface;
   dependentPriceSelected: ViewItemsRadioSelectedStateInterface | ViewItemsCheckboxSelectedStateInterface | null;
  }) {
-  const buttons = {
-    buttons: [
-      {
-        text: "First Half"
-      }, {
-        text: "Second Half"
-      }, {
-        text: "Whole Pizza"
-      }
-    ],
-    default: 2
-  }
+  // console.log("ViewItemCheckbox -> modifier, modifier_option", modifier, modifier_option);
 
-  const [selectedBtn, setSelectedBtn] = useState<number | null>(buttons.default)
-  const {
-    modifier_id,
-    max_selection
-  } = modifier
-  const {
-    option_id,
-    name: option_name,
-    display_text: option_text,
-    price,
+  if (modifier && modifier_option) {
 
-  } = modifier_option
-  const checkboxSelected = selected?.selected_ids[option_id]?.selected
-  // const checkboxSelected = true
-  console.log(selected)
-  let totalSelected = 0
-
-  if (selected.selected_ids) {
-    for (let [option_id, option] of Object.entries(selected?.selected_ids) ) {
-      console.log(option_id)
-      // if (option.selected === true) {
-      //   totalSelected += 1;
-      // }
-    }
-  }
-
-  const disabled = totalSelected === max_selection && !checkboxSelected
-
-  return (
-    <div className="flex flex-col items-start justify-start gap-2">
-      <div className={`flex items-center justify-start gap-2 ${disabled ? `hover:cursor-not-allowed` : `hover:cursor-pointer`}`} onClick={() => {
-        if (!disabled) {
-          handleChange(modifier_id, option_id, price)
-        }
-      }}>
-        <Checkbox 
-          id={option_name} 
-          checked={checkboxSelected}
-          disabled={disabled}
-        />
-        <Label htmlFor={option_name} className="hover:cursor-pointer">{option_text}</Label>
+    const buttons = {
+      buttons: [
         {
-          price &&
-          <div className={`${disabled && `cursor-not-allowed disabled`}`}>
-            {
-              dependentPriceSelected?.type === "RADIO" ? 
-              `${formatCurrency(price[dependentPriceSelected.selected_id || -1])}` : 
-              dependentPriceSelected?.type === "CHECKBOX" &&
-              `Haven't gotten here yet`
-            }
-            {/* {`${formatCurrency(price[(selected[price.modifier_id] as ViewItemsRadioSelectedStateInterface).selected_id || -1])}`} */}
-          </div>
+          text: "First Half"
+        }, {
+          text: "Second Half"
+        }, {
+          text: "Whole Pizza"
         }
+      ],
+      default: 2
+    }
+  
+    const [selectedBtn, setSelectedBtn] = useState<number | null>(buttons.default)
+    const {
+      modifier_id,
+      max_selection
+    } = modifier
+    const {
+      option_id,
+      name: option_name,
+      display_text: option_text,
+      price,
+  
+    } = modifier_option
+    const checkboxSelected = option_id in selected?.selected_ids
+    // console.log("Selected checkbox: ", checkboxSelected)
+    // console.log("Selected items: ", selected)
+    // const checkboxSelected = true
+    // console.log(selected)
+    let totalSelected = 0
+  
+    if (selected.selected_ids) {
+      for (let [option_id, option] of Object.entries(selected?.selected_ids) ) {
+        // console.log(option_id)
+        // if (option.selected === true) {
+          totalSelected += 1;
+        // }
+      }
+    }
+
+    // console.log(dependentPriceSelected)
+  
+    const disabled = totalSelected === max_selection && !checkboxSelected
+  
+    return (
+      <div className="flex flex-col items-start justify-start gap-2">
+        <div className={`flex items-center justify-start gap-2 ${disabled ? `hover:cursor-not-allowed` : `hover:cursor-pointer`}`} onClick={() => {
+          if (!disabled) {
+            handleChange(modifier_id, option_id, option_text, price)
+          }
+        }}>
+          <Checkbox 
+            id={option_name} 
+            checked={checkboxSelected}
+            disabled={disabled}
+          />
+          <Label htmlFor={option_name} className="hover:cursor-pointer">{option_text}</Label>
+          {
+            price &&
+            <div className={`${disabled && `cursor-not-allowed disabled`}`}>
+              {
+                dependentPriceSelected?.type === "RADIO" && typeof price !== "number" ? 
+                `${formatCurrency(price[dependentPriceSelected.selected_id || -1])}` : 
+                dependentPriceSelected?.type === "CHECKBOX" &&
+                `Haven't gotten here yet`
+              }
+              {/* {`${formatCurrency(price[(selected[price.modifier_id] as ViewItemsRadioSelectedStateInterface).selected_id || -1])}`} */}
+            </div>
+          }
+        </div>
+        {checkboxSelected && 
+        <div className="flex gap-4 bg-slate-600 p-2.5 rounded-full">
+          {buttons.buttons.map((val, index) => {
+            return(
+              <button id={`${index}`} onClick={() => setSelectedBtn(index)}>
+                <p className={`p-2.5 ${selectedBtn === index && `bg-slate-400 rounded-full`}`}>
+                  {val.text}
+                </p>
+              </button>
+            )
+          })}
+          </div>}
       </div>
-      {checkboxSelected && 
-      <div className="flex gap-4 bg-slate-600 p-2.5 rounded-full">
-        {buttons.buttons.map((val, index) => {
-          return(
-            <button id={`${index}`} onClick={() => setSelectedBtn(index)}>
-              <p className={`p-2.5 ${selectedBtn === index && `bg-slate-400 rounded-full`}`}>
-                {val.text}
-              </p>
-            </button>
-          )
-        })}
-        </div>}
-    </div>
-  )
+    )
+  }
+  return null
 }
