4982e02be49230f89cb8b2a1f64ef678c3a8c3ac
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-11 15:50:22 -0400

    setup simple server on port 3000

--- backend_test/main.go
+++ backend_test/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+var print = fmt.Println
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", handleGet)
+
+	print("Listening on port 3000")
+	http.ListenAndServe(":3000", r)
 }
