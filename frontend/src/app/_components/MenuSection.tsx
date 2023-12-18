2e12657bf4b272ec614ffcd714272089e93cc326
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2023-12-17 19:08:18 -0500

    Create component for each individual section

---
+++ frontend/src/app/_components/MenuSection.tsx
@@ -0,0 +1,21 @@
+import { MenuSectionPropsType } from "../types";
+import { MenuItem } from "./MenuItem";
+
+// MenuSection will be each individual section, such as Pizza
+export function MenuSection({ section, index }: MenuSectionPropsType ) {
+  console.log(section)
+  return(
+    <div key={index}>
+      <p className="text-primary text-lg font-bold">{section?.DisplayName?.toUpperCase()}</p>
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-4 pt-4"> {/* Make this a flex container with wrap */}
+        <>
+        {section?.items && 
+          section.items.map((item: any, _: any) => {
+          console.log(item)
+          return <MenuItem item={item} />
+        })}
+        </>
+      </div>
+    </div>
+  )
+}
