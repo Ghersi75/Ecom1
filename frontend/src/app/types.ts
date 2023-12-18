66cf58b5a6d65b238dbeaefed8e44615017a97d7
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2023-12-17 19:08:40 -0500

    Added prop types

--- frontend/src/app/types.ts
+++ frontend/src/app/types.ts
@@ -1,3 +1,18 @@
+import { MenuSectionsType } from "@/lib/types/databaseReturnTypes";
+
 export type SearchParamsType = {
   searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export type SectionListPropsType = {
+  sections: MenuSectionsType[] | null
+}
+
+export type MenuSectionPropsType = {
+  section: MenuSectionsType,
+  index: number
+}
+
+export type MenuItemPropsType = {
+  item: any
 }
