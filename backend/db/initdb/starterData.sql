7db487f453e66041dc7aedf00b1628bd530be172
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-09-03 17:19:50 -0400

    Updated starterData to snake_case

--- backend/db/initdb/starterData.sql
+++ backend/db/initdb/starterData.sql
@@ -1,183 +1,180 @@
-CREATE DATABASE PizzeriaTest;
-USE PizzeriaTest;
+CREATE DATABASE pizzeria_test;
+USE pizzeria_test;
 
 -- Main User Info
-CREATE TABLE Users (
-  UserID INT NOT NULL AUTO_INCREMENT,
-  Username VARCHAR(50) NOT NULL UNIQUE,
-  FirstName VARCHAR(50),
-  LastName VARCHAR(50),
+CREATE TABLE users (
+  user_id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
   -- Optional PFP
-  ProfilePicture VARCHAR(255),
-  Email VARCHAR(255) NOT NULL UNIQUE,
-  PhoneNumber VARCHAR(25),
+  profile_picture VARCHAR(255),
+  email VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(25) NOT NULL,
   -- Optional because OAuth doesnt have password
-  PasswordHash VARCHAR(255),
+  password_hash VARCHAR(255),
   -- OAuth, Site Login, or Other
-  Provider VARCHAR(50) NOT NULL,
-  RegisterAt DATETIME,
-  LastLogin DATETIME,
-  PRIMARY KEY (UserID)
+  provider VARCHAR(50),
+  registered_at DATETIME,
+  last_login DATETIME,
+  PRIMARY KEY (user_id)
 );
 
-CREATE TABLE Addresses (
-  AddressID INT NOT NULL AUTO_INCREMENT,
-  UserID INT NOT NULL,
-  State VARCHAR(20) NOT NULL,
-  City VARCHAR(50) NOT NULL,
-  StreetAddress VARCHAR(100) NOT NULL,
-  ZipCode VARCHAR(10) NOT NULL,
-  IsDefault BOOLEAN NOT NULL,
-  PRIMARY KEY (AddressId),
-  FOREIGN KEY (UserID) REFERENCES Users (UserID)
+CREATE TABLE addresses (
+  address_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  state VARCHAR(20) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  street_address VARCHAR(100) NOT NULL,
+  zip_code VARCHAR(10) NOT NULL,
+  is_default BOOLEAN NOT NULL,
+  PRIMARY KEY (address_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
-CREATE TABLE Orders (
-  OrderID INT NOT NULL AUTO_INCREMENT,
-  UserID INT NOT NULL,
-  TotalPrice MEDIUMINT NOT NULL,
-  CheckoutTime DATETIME NOT NULL,
+CREATE TABLE orders (
+  order_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  total_price MEDIUMINT NOT NULL,
+  checkout_time DATETIME NOT NULL,
   -- Fix This
-  DeliveryMethod ENUM("Pickup", "Delivery") NOT NULL,
-  Instructions VARCHAR(500),
-  Status VARCHAR(50) NOT NULL,
-  PRIMARY KEY (OrderID),
-  FOREIGN KEY (UserID) REFERENCES Users (UserID)
+  delivery_method ENUM("Pickup", "Delivery") NOT NULL,
+  instructions VARCHAR(500),
+  status VARCHAR(50) NOT NULL,
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
-CREATE TABLE Modifiers (
-  ModifierID INT NOT NULL AUTO_INCREMENT,
-  Name VARCHAR(100) NOT NULL,
-  DisplayText VARCHAR(100) NOT NULL,
-  Description VARCHAR(255),
-  IsRequired BOOLEAN NOT NULL,
-  MaxSelection INT,
+CREATE TABLE modifiers (
+  modifier_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  display_text VARCHAR(100) NOT NULL,
+  is_required BOOLEAN NOT NULL,
+  max_selection INT,
   -- Double check this
-  ModifierType ENUM("RADIO", "CHECKBOX"),
-  DefaultOptionID INT,
-  DisplayOrder INT NOT NULL,
-  IsActive BOOLEAN NOT NULL,
-  IsAvailable BOOLEAN NOT NULL,
-  PRIMARY KEY (ModifierID)
+  modifier_type ENUM("RADIO", "CHECKBOX"),
+  default_option_id INT,
+  display_order INT NOT NULL,
+  is_active BOOLEAN NOT NULL,
+  is_available BOOLEAN NOT NULL,
+  description VARCHAR(255),
+  PRIMARY KEY (modifier_id)
 );
 
-CREATE TABLE Sections (
-  SectionID INT NOT NULL AUTO_INCREMENT,
-  Name VARCHAR(100) NOT NULL,
-  DisplayName VARCHAR(100) NOT NULL,
-  DisplayOrder INT NOT NULL,
-  IsActive BOOLEAN NOT NULL,
-  IsAvailable BOOLEAN NOT NULL,
-  PRIMARY KEY (SectionID)
+CREATE TABLE sections (
+  section_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  display_name VARCHAR(100) NOT NULL,
+  display_order INT NOT NULL,
+  is_active BOOLEAN NOT NULL,
+  is_available BOOLEAN NOT NULL,
+  PRIMARY KEY (section_id)
 );
 
-CREATE TABLE MenuItems (
-  ItemID INT NOT NULL AUTO_INCREMENT,
-  Name VARCHAR(100) NOT NULL,
-  BasePrice MEDIUMINT,
-  -- If there's no base price, and the current item has a small, medium, and large price, this will display the small price on the landing page
-  DisplayPrice MEDIUMINT,
-  DisplayText VARCHAR(100) NOT NULL,
-  Description VARCHAR(500),
-  ImageLink VARCHAR(2048),
-  IsActive BOOLEAN NOT NULL,
-  IsAvailable BOOLEAN NOT NULL,
-  DisplayOrder INT NOT NULL,
-  IsFeatured BOOLEAN NOT NULL DEFAULT false,
-  PRIMARY KEY (ItemID)
+CREATE TABLE menu_items (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  base_price MEDIUMINT,
+  description VARCHAR(500),
+  image_link VARCHAR(2048),
+  display_text VARCHAR(100),
+  display_price MEDIUMINT,
+  is_active BOOLEAN NOT NULL,
+  is_available BOOLEAN NOT NULL,
+  display_order INT NOT NULL,
+  is_featured BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (item_id)
 );
 
-CREATE TABLE SectionItems (
-  ComboID INT NOT NULL AUTO_INCREMENT,
-  SectionID INT NOT NULL,
-  ItemID INT NOT NULL,
-  DisplayOrder INT NOT NULL, 
-  IsActive BOOLEAN NOT NULL,
-  IsAvailable BOOLEAN NOT NULL,
-  PRIMARY KEY (ComboID),
-  FOREIGN KEY (SectionID) REFERENCES Sections (SectionID),
-  FOREIGN KEY (ItemID) REFERENCES MenuItems (ItemID)
+CREATE TABLE section_items (
+  combo_id INT NOT NULL AUTO_INCREMENT,
+  section_id INT NOT NULL,
+  item_id INT NOT NULL,
+  display_order INT NOT NULL, 
+  is_active BOOLEAN NOT NULL,
+  is_available BOOLEAN NOT NULL,
+  PRIMARY KEY (combo_id),
+  FOREIGN KEY (section_id) REFERENCES sections (section_id),
+  FOREIGN KEY (item_id) REFERENCES menu_items (item_id)
 );
 
-CREATE TABLE SelectionOptions (
-  SelectionOptionID INT NOT NULL AUTO_INCREMENT,
-  OptionName VARCHAR(50) NOT NULL,
-  PRIMARY KEY (SelectionOptionID)
+CREATE TABLE selection_options (
+  selection_option_id INT NOT NULL AUTO_INCREMENT,
+  option_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (selection_option_id)
 );
 
-CREATE TABLE ModifierOptions (
-  OptionID INT NOT NULL AUTO_INCREMENT,
-  ModifierID INT NOT NULL,
-  Name VARCHAR(100) NOT NULL, 
-  DisplayText VARCHAR(50) NOT NULL,
-  DisplayOrder INT NOT NULL,
-  IsActive BOOLEAN NOT NULL,
-  IsAvailable BOOLEAN NOT NULL,
-  BasePrice MEDIUMINT,
-  SelectionOptionID INT,
-  PRIMARY KEY (OptionID),
-  FOREIGN KEY (ModifierID) REFERENCES Modifiers (ModifierID),
-  FOREIGN KEY (SelectionOptionID) REFERENCES SelectionOptions (SelectionOptionID)
+CREATE TABLE modifier_options (
+  option_id INT NOT NULL AUTO_INCREMENT,
+  modifier_id INT NOT NULL,
+  name VARCHAR(100) NOT NULL, 
+  display_text VARCHAR(50) NOT NULL,
+  is_active BOOLEAN NOT NULL,
+  is_available BOOLEAN NOT NULL,
+  base_price MEDIUMINT,
+  selection_option_id INT,
+  PRIMARY KEY (option_id),
+  FOREIGN KEY (modifier_id) REFERENCES modifiers (modifier_id),
+  FOREIGN KEY (selection_option_id) REFERENCES selection_options (selection_option_id)
 );
 
-CREATE TABLE OptionOptionPrices (
-  ComboID INT NOT NULL AUTO_INCREMENT,
-  ParentOptionID INT NOT NULL,
-  DependentOptionID INT NOT NULL,
-  Price MEDIUMINT NOT NULL,
-  IsActive BOOLEAN NOT NULL,
-  IsAvailable BOOLEAN NOT NULL,
-  PRIMARY KEY (ComboID),
-  FOREIGN KEY (ParentOptionID) REFERENCES ModifierOptions (OptionID),
-  FOREIGN KEY (DependentOptionID) REFERENCES ModifierOptions (OptionID)
+CREATE TABLE option_option_prices (
+  combo_id INT NOT NULL AUTO_INCREMENT,
+  parent_option_id INT NOT NULL,
+  dependent_option_id INT NOT NULL,
+  price MEDIUMINT NOT NULL,
+  is_active BOOLEAN NOT NULL,
+  is_available BOOLEAN NOT NULL,
+  PRIMARY KEY (combo_id),
+  FOREIGN KEY (parent_option_id) REFERENCES modifier_options (option_id),
+  FOREIGN KEY (dependent_option_id) REFERENCES modifier_options (option_id)
 );
 
-CREATE TABLE ItemModifiers (
-  ComboID INT NOT NULL AUTO_INCREMENT,
-  ItemID INT NULL,
-  ModifierID INT NULL,
-  DisplayOrder INT NOT NULL, 
-  IsActive BOOLEAN NOT NULL,
-  IsAvailable BOOLEAN NOT NULL,
-  PRIMARY KEY (ComboID),
-  FOREIGN KEY (ItemID) REFERENCES MenuItems (ItemID),
-  FOREIGN KEY (ModifierID) REFERENCES Modifiers (ModifierID)
+CREATE TABLE item_modifiers (
+  combo_id INT NOT NULL AUTO_INCREMENT,
+  item_id INT NULL,
+  modifier_id INT NULL,
+  display_order INT NOT NULL, 
+  is_active BOOLEAN NOT NULL,
+  is_available BOOLEAN NOT NULL,
+  PRIMARY KEY (combo_id),
+  FOREIGN KEY (item_id) REFERENCES menu_items (item_id),
+  FOREIGN KEY (modifier_id) REFERENCES modifiers (modifier_id)
 );
 
-CREATE TABLE ItemOptionPrices (
-  ComboID INT NOT NULL AUTO_INCREMENT,
-  ItemID INT NOT NULL,
-  OptionID INT NOT NULL,
-  Price MEDIUMINT NOT NULL,
-  IsActive BOOLEAN NOT NULL,
-  IsAvailable BOOLEAN NOT NULL,
-  PRIMARY KEY (ComboID),
-  FOREIGN KEY (ItemID) REFERENCES MenuItems (ItemID),
-  FOREIGN KEY (OptionID) REFERENCES ModifierOptions (OptionID)
+CREATE TABLE item_option_prices (
+  combo_id INT NOT NULL AUTO_INCREMENT,
+  item_id INT NOT NULL,
+  option_id INT NOT NULL,
+  price MEDIUMINT NOT NULL,
+  is_active BOOLEAN NOT NULL,
+  is_available BOOLEAN NOT NULL,
+  PRIMARY KEY (combo_id),
+  FOREIGN KEY (item_id) REFERENCES menu_items (item_id),
+  FOREIGN KEY (option_id) REFERENCES modifier_options (option_id)
 );
 
-CREATE TABLE OrderItems (
-  OrderItemID INT NOT NULL AUTO_INCREMENT,
-  OrderID INT NOT NULL,
-  ItemID INT NOT NULL,
-  ItemPrice MEDIUMINT NOT NULL,
-  PRIMARY KEY (OrderItemID),
-  FOREIGN KEY (OrderID) REFERENCES Orders (OrderID),
-  FOREIGN KEY (ItemID) REFERENCES MenuItems (ItemID)
+CREATE TABLE order_items (
+  order_item_id INT NOT NULL AUTO_INCREMENT,
+  order_id INT NOT NULL,
+  item_id INT NOT NULL,
+  item_price MEDIUMINT NOT NULL,
+  PRIMARY KEY (order_item_id),
+  FOREIGN KEY (order_id) REFERENCES orders (order_id),
+  FOREIGN KEY (item_id) REFERENCES menu_items (item_id)
 );
 
-CREATE TABLE OrderItemModifiers (
-  OrderItemModifierID INT NOT NULL AUTO_INCREMENT,
-  OrderItemID INT NOT NULL,
-  ModifierOptionID INT NOT NULL,
-  SelectionOptionID INT,
-  PRIMARY KEY (OrderItemModifierID),
-  FOREIGN KEY (OrderItemID) REFERENCES OrderItems (OrderItemID),
-  FOREIGN KEY (ModifierOptionID) REFERENCES ModifierOptions (OptionID),
-  FOREIGN KEY (SelectionOptionID) REFERENCES SelectionOptions (SelectionOptionID)
+CREATE TABLE order_item_modifiers (
+  order_item_modifier_id INT NOT NULL AUTO_INCREMENT,
+  order_item_id INT NOT NULL,
+  modifier_option_id INT NOT NULL,
+  selection_option_id INT,
+  PRIMARY KEY (order_item_modifier_id),
+  FOREIGN KEY (order_item_id) REFERENCES order_items (order_item_id),
+  FOREIGN KEY (modifier_option_id) REFERENCES modifier_options (option_id),
+  FOREIGN KEY (selection_option_id) REFERENCES selection_options (selection_option_id)
 );
 
-INSERT INTO Sections (Name, DisplayOrder, IsActive, IsAvailable, DisplayName) VALUES 
+INSERT INTO sections (name, display_order, is_active, is_available, display_name) VALUES 
   ("deals", 0, 1, 1, "Deals and Coupons"), 
   ("popular", 1, 1, 1, "Popular Items"), 
   ("specials", 2, 1, 1, "Specials"), 
@@ -195,10 +192,10 @@ INSERT INTO Sections (Name, DisplayOrder, IsActive, IsAvailable, DisplayName) VA
   ("desserts", 14, 1, 1, "Desserts"), 
   ("drinks", 15, 1, 1, "Drinks");
 
-INSERT INTO MenuItems (Name, Description, ImageLink, BasePrice, DisplayOrder, IsActive, IsAvailable, DisplayText, DisplayPrice) VALUES 
+INSERT INTO menu_items (name, description, image_link, base_price, display_order, is_active, is_available, display_text, display_price) VALUES 
   ("pizza_specialty_buffalo", "Our homemade buffalo chicken pizza. White pizza topped with buffalo chicken, our special blend of mozzarella cheese, and blue cheese as a sauce substitute.", "https://img.freepik.com/premium-photo/aesthetic-dripping-tasty-pizza-slice-generative-ai_863013-1954.jpg", NULL, 0, 1, 1, "Buffalo Chicken Pizza", 1699), 
   ("pizza_specialty_margherita", "Our homemade buffalo margherita pizza. White pizza topped with fresh tomatoes, fresh basil, and fresh garlic", "https://img.freepik.com/premium-photo/aesthetic-dripping-tasty-pizza-slice-generative-ai_863013-1954.jpg", NULL, 1, 1, 1, "Margherita Pizza", 1399);
 
-INSERT INTO SectionItems (DisplayOrder, IsActive, IsAvailable, SectionID, ItemID) VALUES 
+INSERT INTO section_items (display_order, is_active, is_available, section_id, item_id) VALUES 
   (0, 1, 1, 2, 1), 
   (1, 1, 1, 2, 2);
